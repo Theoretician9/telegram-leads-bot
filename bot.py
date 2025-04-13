4804759707666a1557e50b362a3c790e5626742e
Nikita Fedosseyev <nikita.f3d@gmail.com>
2025-04-14 00:55:10 +0500

    Fix path to credentials.json

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ scope = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive"
 ]
-creds = ServiceAccountCredentials.from_json_keyfile_name("optical-legend-266113-e3bbc97c1f8a.json", scope)
+ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scope)
 client = gspread.authorize(creds)
 
 # 📄 Подключаемся к таблице
